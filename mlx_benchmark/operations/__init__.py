123af9d8338fe590d3061469407cb4bc2ef5d587
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-01-04 00:04:00 +0100

    ✨ add conv1d

--- mlx_benchmark/operations/__init__.py
+++ mlx_benchmark/operations/__init__.py
@@ -1,5 +1,5 @@
 from .linear import Linear
-from .conv2d import Conv2d
+from .conv import Conv1d, Conv2d
 from .matmul import MatMul
 from .binary_cross_entropy import BCE
 from .concat import Concat
--- mlx_benchmark/operations/conv.py
+++ mlx_benchmark/operations/conv.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 from base_benchmark import BaseBenchmark
 
 
-class Conv2d(BaseBenchmark):
+class Conv1d(BaseBenchmark):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -19,6 +19,31 @@ class Conv2d(BaseBenchmark):
         a, b = self.inputs
 
         # In torch, the channels are located at axis 1
+        if framework == "torch":
+            self.a_torch = torch.transpose(a, -1, -2)
+            self.b_torch = torch.transpose(b, -1, -2)
+
+    def forward_mlx(self, **kwargs):
+        a, b = self.inputs
+
+        y = mx.conv1d(a, b)
+        mx.eval(y)
+
+    @torch.no_grad()
+    def forward_torch(self, **kwargs):
+        a, b = self.a_torch, self.b_torch
+
+        y = F.conv1d(a, b)
+        self.sync_mps_if_needed()
+
+
+class Conv2d(BaseBenchmark):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def additional_preprocessing(self, framework):
+        a, b = self.inputs
+
         if framework == "torch":
             self.a_torch = torch.permute(a, (0, 3, 1, 2))
             self.b_torch = torch.permute(b, (0, 3, 1, 2))
